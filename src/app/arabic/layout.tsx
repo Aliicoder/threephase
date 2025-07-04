29f4f05ef9ab75882303ab225a355a602871d77a
ali_fahmi <k.c.o.c.3000@gmail.com>
2025-07-04 18:34:17 +0530

    0-0.2.6 setup some SEO

--- src/app/arabic/layout.tsx
+++ src/app/arabic/layout.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "ThreePhase",
+  title: "ThreePhase - film electricity solutions",
   description:
     "تقدم ThreePhase حلولاً كهربائية متكاملة ثلاثية الطور لمواقع التصوير السينمائي، الاستوديوهات الإنتاجية، والمنشآت الترفيهية. نوفر أنظمة توزيع طاقة آمنة، حلول كهرباء مؤقتة، وإدارة طاقة فعالة لضمان سير عمليات الإنتاج دون انقطاع. فريقنا الخبير يضمن توفير حلول طاقة متوافقة مع المعايير العالمية لقطاع الترفيه.",
   keywords:
